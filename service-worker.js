957615340508149b0b773340bdc3b7f47ae03131
Shadman Shakib Sium <shadmanshakibsium@gmail.com>
2026-03-27 19:12:19 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,13 @@
-const CACHE_NAME = 'song-archive-' + Date.now();
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json'
-];
+const CACHE_NAME = 'song-archive-v1';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   );
   self.clients.claim();
@@ -25,6 +15,8 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
   );
 });
