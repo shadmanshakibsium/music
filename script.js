329d38c2c86e4fc43263c38acd8c5f4f864f54ed
Shadman Shakib Sium <shadmanshakibsium@gmail.com>
2026-04-05 22:39:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1758,7 +1758,7 @@ aboutModal.addEventListener('click', (e) => {
         tabsEl.style.paddingBottom = '8px';
         tabsEl.style.borderRadius = '0 0 16px 16px';
 
-        const gap = h + 8;
+        const gap = h;
         musicListEl.style.paddingTop = gap + 'px';
         folderListEl.style.paddingTop = gap + 'px';
         genreListEl.style.paddingTop = gap + 'px';
